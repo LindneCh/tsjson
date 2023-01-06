e22a0d1f218330c23daecec247007684bb1504b1
LindneCh <lindner.christoph01@gmail.com>
2023-01-06 01:57:10 +0100

    Speichern unfertig

--- json.py
+++ json.py
@@ -1,3 +1,12 @@
 import json
+print("")
+print("Aufgabe json wir ausgegeben: ")
+print("")
+#data = json.load(open("accounts.json"))
 
-print(json)
+with open("tsjson/accounts.json", "r") as f:
+  f = json.load(f)
+  print(f)
+  #print(json.dumps(json.load(f), indent=4))
+
+## Fehler ausgabe, warum? vielleicht Klammernproblem in accounts.json {}
